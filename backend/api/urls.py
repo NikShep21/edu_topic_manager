14d80193e01c46fd688466a0da1bea48c5328a40
Max <moximka06@gmail.com>
2026-03-06 17:08:50 +0800

    feat(api): register users and topics routes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,10 @@
 from rest_framework.routers import DefaultRouter
+from users.views import UserViewSet
+from topics.views import TopicViewSet
 
 router = DefaultRouter()
+
+router.register("users", UserViewSet)
+router.register("topics", TopicViewSet)
 
 urlpatterns = router.urls
