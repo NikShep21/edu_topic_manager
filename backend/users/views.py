c60feb0a2ae7ef06a77eb27504b7652d94945182
Max <moximka06@gmail.com>
2026-03-21 18:04:08 +0800

    chore: add UserViewset in users

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
+from .permissions import IsAdminRole
 
 User = get_user_model()
 
@@ -8,3 +9,4 @@ User = get_user_model()
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAdminRole]
