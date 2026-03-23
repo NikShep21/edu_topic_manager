b20b239280d6bdd39040e6a475f2aba917d38d84
NikShep21 <klimenko.leva.lol2@yandex.ru>
2026-03-23 15:15:03 +0800

    feat: add devtools query

--- frontend/src/app/_providers/AppProviders.tsx
+++ frontend/src/app/_providers/AppProviders.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "./ThemeProvider";
 import { queryClient } from "../../shared/api/react-query/query-client";
@@ -10,6 +11,7 @@ export const AppProviders = ({ children }: { children: React.ReactNode }) => {
     <QueryClientProvider client={queryClient}>
       <ThemeProvider attribute="data-theme" defaultTheme="light" enableSystem={false}>
         {children}
+        <ReactQueryDevtools initialIsOpen={false} />
       </ThemeProvider>
     </QueryClientProvider>
   );
