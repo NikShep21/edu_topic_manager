6ff4e0eda8820313ca1e3e83099f6590c3f33596
NikShep21 <klimenko.leva.lol2@yandex.ru>
2026-03-23 15:17:25 +0800

    fix: more types in user

--- frontend/src/entities/user/model/types.ts
+++ frontend/src/entities/user/model/types.ts
@@ -1,7 +1,9 @@
 export type UserRole = "Admin" | "Student" | "Teacher";
 
 export interface UserData {
+  id: number;
   firstName: string;
   lastName: string;
+  middle_name: string;
   role: UserRole;
 }
