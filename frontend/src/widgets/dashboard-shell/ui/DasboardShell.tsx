1535e23f7c1c89fc53a025b7c668a038fac4df9e
nikshep <klimenko.leva.lol2@yandex.ru>
2026-04-02 03:05:05 +0800

    feat: updating profile add logout

--- frontend/src/widgets/dashboard-shell/ui/DasboardShell.tsx
+++ frontend/src/widgets/dashboard-shell/ui/DasboardShell.tsx
@@ -5,6 +5,7 @@ import { Sidebar } from "@/widgets/sidebar/ui/Sidebar";
 import { Header } from "@/widgets/header";
 import styles from "./DashboardShell.module.scss";
 import type { SidebarNavItem } from "@/widgets/sidebar";
+import { useUserQuery } from "@/entities/user";
 
 interface DashboardShellProps {
   children: React.ReactNode;
@@ -18,9 +19,16 @@ export const DashboardShell = ({ children, nav, title }: DashboardShellProps) =>
   const openSidebar = () => setIsSidebarOpen(true);
   const closeSidebar = () => setIsSidebarOpen(false);
 
+  const { data: userData } = useUserQuery();
+
   return (
     <div className={styles.layout}>
-      <Sidebar nav={nav} isOpen={isSidebarOpen} onClose={closeSidebar} />
+      <Sidebar
+        nav={nav}
+        isOpen={isSidebarOpen}
+        onClose={closeSidebar}
+        userData={userData}
+      />
       <div className={styles.page}>
         <Header onOpenSidebar={openSidebar} title={title} />
         <main className={styles.content}>{children}</main>
--- frontend/src/widgets/sidebar/ui/Sidebar.tsx
+++ frontend/src/widgets/sidebar/ui/Sidebar.tsx
@@ -1,25 +1,28 @@
 "use client";
-
+import { IoLogOut } from "react-icons/io5";
 import Link from "next/link";
 import clsx from "clsx";
 import { usePathname } from "next/navigation";
 import { RxCross2 } from "react-icons/rx";
 import { PiStudentFill } from "react-icons/pi";
-
+import { CgProfile } from "react-icons/cg";
 import styles from "./Sidebar.module.scss";
 import { NavLink } from "@/shared/ui/nav-link";
 import type { SidebarNavItem } from "../model/types";
-import { UserBadge } from "@/entities/user";
+import { UserBadge, type UserData } from "@/entities/user";
+import { Dropdown, DropdownMenu, DropdownMenuItem } from "@/shared/ui/dropdown";
+import { useLogout } from "@/features/logout";
 
 interface SidebarProps {
   nav: SidebarNavItem[];
   isOpen: boolean;
   onClose: () => void;
+  userData?: UserData;
 }
 
-export const Sidebar = ({ nav, isOpen, onClose }: SidebarProps) => {
+export const Sidebar = ({ nav, isOpen, onClose, userData }: SidebarProps) => {
   const pathname = usePathname();
-
+  const { logout, isPending } = useLogout();
   return (
     <>
       <button
@@ -79,7 +82,34 @@ export const Sidebar = ({ nav, isOpen, onClose }: SidebarProps) => {
         </div>
         <div className={styles.bottomPart}>
           <div className={styles.sidebarFooter}>
-            <UserBadge firstName={"Лев"} lastName={"Клименко"} role="admin" />
+            <Dropdown
+              placement="top-center"
+              className={styles.userDropdown}
+              trigger={
+                <button
+                  type="button"
+                  className={styles.userButton}
+                  aria-label="User menu"
+                >
+                  <UserBadge userData={userData} />
+                </button>
+              }
+            >
+              <DropdownMenu>
+                <DropdownMenuItem icon={<CgProfile size={17} />}>
+                  Профиль
+                </DropdownMenuItem>
+
+                <DropdownMenuItem
+                  onClick={logout}
+                  disabled={isPending}
+                  icon={<IoLogOut size={17} />}
+                  danger
+                >
+                  Выйти
+                </DropdownMenuItem>
+              </DropdownMenu>
+            </Dropdown>
           </div>
         </div>
       </aside>
