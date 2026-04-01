30a76644ac416e3bf9c8dd38c3b81aea9e7d9b97
Max <moximka06@gmail.com>
2026-04-02 02:54:54 +0800

    chore: change blank to False in firstname, lastname and middlename

--- backend/users/models.py
+++ backend/users/models.py
@@ -43,17 +43,26 @@ class User(AbstractUser):
         STUDENT = "student", _("Студент")
         TEACHER = "teacher", _("Преподаватель")
         ADMIN = "admin", _("Администратор")
-
+    first_name = models.CharField(
+        max_length=150,
+        blank=False,
+        verbose_name=_("Имя"),
+    )
+    last_name = models.CharField(
+        max_length=150,
+        blank=False,
+        verbose_name=_("Фамилия"),
+    )
+    middle_name = models.CharField(
+        max_length=150,
+        blank=False,
+        verbose_name=_("Отчество"),
+    )
     role = models.CharField(
         max_length=10,
         choices=Role.choices,
         default=Role.STUDENT,
         verbose_name=_("Роль"),
-    )
-    middle_name = models.CharField(
-        max_length=150,
-        blank=True,
-        verbose_name=_("Отчество"),
     )
     email = models.EmailField(
         _("email address"),
