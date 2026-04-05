c8cbf9a03d1381656b85a4ad239976377c8cad71
nikshep <klimenko.leva.lol2@yandex.ru>
2026-04-06 04:18:13 +0800

    feat: new base page with table

--- frontend/src/app/admin/students/page.tsx
+++ frontend/src/app/admin/students/page.tsx
@@ -1,11 +1,14 @@
+import { StudentsManagement } from "@/widgets/students-management";
 import styles from "./page.module.scss";
 
-const page = () => {
+const Page = () => {
   return (
     <main className={styles.content}>
-      <section className={styles.tableContainer}></section>
+      <section className={styles.tableContainer}>
+        <StudentsManagement />
+      </section>
     </main>
   );
 };
 
-export default page;
+export default Page;
