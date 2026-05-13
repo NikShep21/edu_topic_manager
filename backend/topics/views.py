b28411b4854e4e299b10fa2545a679a24bf769d5
Max <moximka06@gmail.com>
2026-05-14 03:40:25 +0800

    fix: views topics update delete files

--- backend/topics/views.py
+++ backend/topics/views.py
@@ -15,27 +15,43 @@ class TopicViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         user = self.request.user
 
+        queryset = Topic.objects.select_related(
+            "teacher",
+            "student",
+            "student__student_profile",
+            "student__student_profile__group",
+        ).prefetch_related(
+            "steps",
+            "files",
+            "applications",
+        )
+
         if user.role == "teacher":
-            qs = Topic.objects.filter(teacher=user)
+            queryset = queryset.filter(teacher=user)
             # фильтры по типу и статусу
             type_param = self.request.query_params.get("type")
             status_param = self.request.query_params.get("status")
-            if type_param in [Topic.Type.COURSEWORK, Topic.Type.VKR]:
-                qs = qs.filter(type=type_param)
-            if status_param in [Topic.Status.AVAILABLE, Topic.Status.PENDING_APPROVAL, Topic.Status.ASSIGNED]:
-                qs = qs.filter(status=status_param)
             search = self.request.query_params.get("search")
+
+            if type_param in [Topic.Type.COURSEWORK, Topic.Type.VKR]:
+                queryset = queryset.filter(type=type_param)
+
+            if status_param in [Topic.Status.AVAILABLE, Topic.Status.PENDING_APPROVAL, Topic.Status.ASSIGNED]:
+                queryset = queryset.filter(status=status_param)
+                
             if search:
-                qs = qs.filter(title__icontains=search)
-            return qs
+                queryset = queryset.filter(title__icontains=search)
+
+            return queryset
 
         elif user.role == "student":
             # автофильтр по курсу
-            course = getattr(user.studentprofile, "course", None)
+            student_profile = getattr(user, "student_profile", None)
+            course = getattr(student_profile, "course", None)
             if course in [1, 2, 3]:
-                qs = Topic.objects.filter(type=Topic.Type.COURSEWORK)
+                queryset = queryset.filter(type=Topic.Type.COURSEWORK)
             elif course == 4:
-                qs = Topic.objects.filter(type=Topic.Type.VKR)
+                queryset = queryset.filter(type=Topic.Type.VKR)
             else:
                 return Topic.objects.none()
 
@@ -43,12 +59,12 @@ class TopicViewSet(viewsets.ModelViewSet):
             teacher_param = self.request.query_params.get("teacher")
             search = self.request.query_params.get("search")
             if status_param in [Topic.Status.AVAILABLE, Topic.Status.PENDING_APPROVAL, Topic.Status.ASSIGNED]:
-                qs = qs.filter(status=status_param)
+                queryset = queryset.filter(status=status_param)
             if teacher_param:
-                qs = qs.filter(teacher_id=teacher_param)
+                queryset = queryset.filter(teacher_id=teacher_param)
             if search:
-                qs = qs.filter(title__icontains=search)
-            return qs
+                queryset = queryset.filter(title__icontains=search)
+            return queryset
 
         return Topic.objects.none()
 
@@ -56,29 +72,31 @@ class TopicViewSet(viewsets.ModelViewSet):
         serializer.save(teacher=self.request.user)
 
     def get_permissions(self):
-        if self.action in ["update", "partial_update", "destroy"]:
-            self.permission_classes = [IsTeacherOwner]
-        return super().get_permissions()
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsTeacherOwner()]
+        return [IsAuthenticated()]
 
-    # Удаление отдельного файла темы
-    @action(detail=True, methods=["delete"], url_path="files/(?P<file_id>[^/.]+)")
-    def delete_file(self, request, pk=None, file_id=None):
+    def destroy(self, request, *args, **kwargs):
         topic = self.get_object()
-        try:
-            file_obj = topic.files.get(id=file_id)
-            file_obj.file.delete(save=False)
-            file_obj.delete()
-            return Response({"text": "Файл удалён"}, status=status.HTTP_204_NO_CONTENT)
-        except TopicFile.DoesNotExist:
-            return Response({"text": "Файл не найден"}, status=status.HTTP_404_NOT_FOUND)
 
-    # Добавление нового файла к теме
-    @action(detail=True, methods=["post"], url_path="files")
-    def add_file(self, request, pk=None):
-        topic = self.get_object()
-        uploaded_file = request.FILES.get("file")
-        if not uploaded_file:
-            return Response({"text": "Файл не передан"}, status=status.HTTP_400_BAD_REQUEST)
-        file_obj = TopicFile.objects.create(topic=topic, file=uploaded_file)
-        serializer = TopicFileSerializer(file_obj)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if topic.status != Topic.Status.AVAILABLE:
+            return Response(
+                {"text": "Можно удалить только свободную тему"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        if topic.applications.exists():
+            return Response(
+                {"text": "Нельзя удалить тему, по которой уже были заявки"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        self.perform_destroy(topic)
+
+        return Response(
+            {
+                "success": True,
+                "message": "Тема удалена",
+            },
+            status=status.HTTP_200_OK
+        )
